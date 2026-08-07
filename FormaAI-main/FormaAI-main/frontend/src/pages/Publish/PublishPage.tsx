c4819520e65751e2e6c0c3c5cf89250db5590d62
Suhasini Nanaware <suhasininanaware19@gmail.com>
2026-08-07 22:11:12 +0530

    Delete FormaAI-main/FormaAI-main directory

--- FormaAI-main/FormaAI-main/frontend/src/pages/Publish/PublishPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const PublishPage: React.FC = () => {
-  return <div className="p-8 text-white">Publish Page (Coming Soon)</div>;
-};
-
-export default PublishPage;
