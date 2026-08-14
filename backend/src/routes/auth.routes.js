de21a1be3cd2c61d81e62edbbcc04cbd1c35cc9d
Dinesh Choudhary <paurdinesh226@gmail.com>
2026-08-14 21:14:39 +0530

    Update email verification

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authController = require("../controllers/auth.controller");
+const authController = require("../controllers/auth_controller");
 
 const {
     registerValidation,
